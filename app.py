faa82e88886331d0c7f673efa685384099eb1dfa
thiago-shibata <thiago.shibatta@gmail.com>
2024-11-12 16:13:33 -0300

    new feat: Method Read of the CRUD - return all elements

--- app.py
+++ app.py
@@ -18,5 +18,16 @@ def create_task():
   print(tasks)
   return jsonify({"message":"Nova tarefa criada com sucesso!"})
 
+@app.route("/tasks", methods=['GET'])
+def get_tasks():
+  task_list = [task.to_dict() for task in tasks]
+  output = {
+          "tasks": task_list,
+          "total_tasks": len(task_list)
+        }
+  
+  return jsonify(output)
+
+
 if __name__ == "__main__":
   app.run(debug=True)
