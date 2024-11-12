622043c527aa0286cf6a9e60b58ba7a7fd7be3cd
thiago-shibata <thiago.shibatta@gmail.com>
2024-11-12 15:45:20 -0300

    new feature: Method create of de CRUD

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from models.task import Task
 # manual: __name = "__main__"
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return "<p>Hello, World!</p>"
+tasks = []
+task_id_control = 1
 
-@app.route("/about")
-def about():  
-  return "<h1>Página sobre</h1>"
+
+
+@app.route("/tasks", methods=['POST'])
+def create_task():
+  global task_id_control # variável global para ser acessada de dentro da função
+  data = request.get_json()
+  new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+  task_id_control += 1
+  tasks.append(new_task)
+  print(tasks)
+  return jsonify({"message":"Nova tarefa criada com sucesso!"})
 
 if __name__ == "__main__":
   app.run(debug=True)
